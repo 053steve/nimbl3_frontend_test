d9a5c82d8f25c2641ba5d4583d9a02224b34c36d
Steve Suranant <053steve@gmail.com>
2017-05-27 16:08:42 +0700

    Changed to Redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,91 +1,26 @@
 import {
     combineReducers
 } from 'redux'
-import {
-    SELECT_REDDIT,
-    INVALIDATE_REDDIT,
-    REQUEST_POSTS,
-    RECEIVE_POSTS,
-    NON_AUTH,    
-    SUCCESS_AUTH
+import {    
+    CHANGE_BAZ
 } from '../actions'
 
-function selectedReddit(state = 'reactjs', action) {    
+function app(state = {}, action) {    
     switch (action.type) {
-        case SELECT_REDDIT:
-            return action.reddit
+        case CHANGE_BAZ:
+            return {
+                ...state,
+                baz: action.payload,
+            };
         default:
             return state
     }
 }
 
-function user(state = {    
-    userProfile: {}
 
-}, action) {
-    
-    switch (action.type) {
-        case NON_AUTH:
 
-            return state = { userProfile: {} }
-        case SUCCESS_AUTH:            
-            return Object.assign({}, state, {                
-                userProfile: action.userPayload,
-                lastUpdated: action.receivedAt
-            })
-        default:
-            return state
-    }
-
-}
-
-function posts(state = {
-    isFetching: false,
-    didInvalidate: false,
-    items: []
-}, action) {
-
-    switch (action.type) {
-        case INVALIDATE_REDDIT:
-            return Object.assign({}, state, {
-                didInvalidate: true
-            })
-        case REQUEST_POSTS:
-            return Object.assign({}, state, {
-                isFetching: true,
-                didInvalidate: false
-            })
-        case RECEIVE_POSTS:
-            return Object.assign({}, state, {
-                isFetching: false,
-                didInvalidate: false,
-                items: action.posts,
-                lastUpdated: action.receivedAt
-            })
-        default:
-            return state
-    }
-}
-
-function postsByReddit(state = {}, action) {
-
-    switch (action.type) {
-        case INVALIDATE_REDDIT:
-        case RECEIVE_POSTS:
-        case REQUEST_POSTS:
-            return Object.assign({}, state, {
-                [action.reddit]: posts(state[action.reddit], action)
-            })
-        default:
-            return state
-    }
-}
-
-const rootReducer = combineReducers({
-    postsByReddit,
-    selectedReddit,
-    posts,
-    user
+const rootReducer = combineReducers({    
+    app    
 })
 
 
