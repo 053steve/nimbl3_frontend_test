12bbeea3f07f8648d808282a0a83b5a319ce5112
Steve Suranant <053steve@gmail.com>
2017-05-30 22:32:16 +0700

    Save before fucking something up.

--- src/components/OrderForm/ProductList/index.js
+++ src/components/OrderForm/ProductList/index.js
@@ -1,52 +1,127 @@
 import React, { Component } from 'react'
+import editIcon from '../../../assets/icons/icon_edit.svg';
+
 
 class ProductList extends Component {  
-    
+
 
   render() {
     
     return  (        
-        <div id="productlist-wrapper" className="columns small-12">
-            <div id="productlist" className="cards">
-                <table>
-                  <thead>
-                    <tr>
-                      <th>sku</th>
-                      <th>description</th>
-                      <th>brand</th>
-                      <th>list price</th>                      
-                      <th>discount</th>
-                      <th>net price</th>
-                      <th>qty</th>
-                      <th>total</th>
-                      <th></th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>TN SO 000015</td>
-                      <td>Magnetic contactor - 9A (5.5 kW\, 7.5 HP)\, control voltage 230 Vac</td>
-                      <td>Schneider Electric</td>
-                      <td>THB 880</td>                      
-                      <td>30%</td>
-                      <td>THB 660</td>
-                      <td>16</td>
-                      <td>THB 50,079,8823</td>
-                      <td>
+        <div id="productlist-wrapper" className="columns small-12"> 
+          <table id="productlist" className="cards">            
+            <thead>
+              <tr>
+                {this.props.theadList.map(function(title) {
+                  return <th key={title}>{title}</th>;
+                })}
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>              
+              {this.props.listObject.map((listobj) => { 
+                  let rows = []
+                  let pricesTd, netTd, totalTd                  
+
+                  if(listobj.listprice) {
+                    pricesTd = <td className="list-price">{listobj.listprice}</td>                      
+                    netTd = <td className="net-price">{listobj.listprice}</td>
+                    totalTd = <td className="total">{listobj.total}</td>
+                  } else {
+                    let alert = <td className="list-price price-set-alert"><a href="#">Set Price</a></td> 
+                    pricesTd = alert
+                    netTd = alert
+                    totalTd = alert
+                  }
+
+                  let tr = <tr>
+                      <td className="sku">{listobj.sku}</td>
+                      <td className="desc">{listobj.desc}</td>
+                      <td className="brand">{listobj.brand}</td>
+                      {pricesTd}
+                      <td className="discount-price">{listobj.discountprice}</td>
+                      {netTd}
+                      <td className="qty">
+                        {listobj.qty} <img src={editIcon} className="icon edit-icon"/>
+                      </td>
+                      {totalTd}
+                      <td className="remove-item">
                         <a href="#">
                           Remove Item
                         </a>
                       </td>                      
-                    </tr>                    
+                    </tr>  
+
+                  rows.push(tr)
                     
-                  </tbody>
-                </table>
-            </div> 
-        </div>         
+                  return rows        
+              })}
+              
+            </tbody>
+          </table>
+      </div> 
+
+                
     );
   }
 }
 
+ProductList.defaultProps = {
+  theadList: ['sku', 'description', 'brand', 'list price', 'discount price', 'net price', 'qty', 'total'],
+  listObject: [
+      {
+        sku: 'TN SO 000015', 
+        desc: 'Magnetic contactor - 9A (5.5 kW\, 7.5 HP)\, control voltage 230 Vac',
+        brand: 'Schneider Electric',
+        listprice: 'THB 880',
+        discountprice: '30%',
+        netprice: 'THB 660',
+        qty: 16,
+        total: 'THB 50,079,8823'
+      },
+      {
+        sku: 'TN SO 000015', 
+        desc: 'Magnetic contactor - 9A (5.5 kW\, 7.5 HP)\, control voltage 230 Vac',
+        brand: 'Schneider Electric',
+        listprice: 'THB 880',
+        discountprice: '30%',
+        netprice: 'THB 660',
+        qty: 16,
+        total: 'THB 50,079,8823'
+      },
+      {
+        sku: 'TN SO 000015', 
+        desc: 'Magnetic contactor - 9A (5.5 kW\, 7.5 HP)\, control voltage 230 Vac',
+        brand: 'Schneider Electric',
+        listprice: 'THB 880',
+        discountprice: '30%',
+        netprice: 'THB 660',
+        qty: 16,
+        total: 'THB 50,079,8823'
+      },
+      {
+        sku: 'TN SO 000015', 
+        desc: 'Magnetic contactor - 9A (5.5 kW\, 7.5 HP)\, control voltage 230 Vac',
+        brand: 'Schneider Electric',
+        listprice: '',
+        discountprice: '30%',
+        netprice: '',
+        qty: 16,
+        total: ''
+      },
+      {
+        sku: 'TN SO 000015', 
+        desc: 'Magnetic contactor - 9A (5.5 kW\, 7.5 HP)\, control voltage 230 Vac',
+        brand: 'Schneider Electric',
+        listprice: 'THB 880',
+        discountprice: '30%',
+        netprice: 'THB 660',
+        qty: 16,
+        total: 'THB 50,079,8823'
+      }
+
+  ]
+}
 
 
 export default ProductList
